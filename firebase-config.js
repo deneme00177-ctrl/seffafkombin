1d9d72fdc5c17545204471a0ce77be50e638acb4
deneme00177-ctrl <deneme00177@gmail.com>
2026-06-29 12:33:22 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+ apiKey: "AIzaSyCEDM36z_akjqyZvtZDF9g-fD_lwPm24Ow",
+  authDomain: "kadir-62bd0.firebaseapp.com",
+  databaseURL: "https://kadir-62bd0-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "kadir-62bd0",
+  storageBucket: "kadir-62bd0.firebasestorage.app",
+  messagingSenderId: "941765355332",
+  appId: "1:941765355332:web:ca71f8df5c63c9c75b1911"
+};
